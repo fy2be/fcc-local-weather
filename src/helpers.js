b73104c48cf246268c7f12d6e974a0af0e7e9599
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-10 16:17:40 +0200

    stupid solution for 'how to check empty object'

--- src/helpers.js
+++ src/helpers.js
@@ -5,3 +5,11 @@ export function fahrenheitToCelsius(fdeg) {
 export function celsiusToFahrenheit(cdeg) {
     return cdeg * 9 / 5 + 32;
 }
+
+export function isEmpty(obj) {
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
