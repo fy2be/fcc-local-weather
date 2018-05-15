97af603d38c538c312980712f77b7d347d2f23a4
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-15 11:05:56 +0200

    ScalePicker is now stateless component

--- src/components/ScalePicker.js
+++ src/components/ScalePicker.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-class ScalePicker extends React.Component {
-    render() {
-        return (
-            <div className='scale-picker' onClick={this.props.handleChangeScale}>
-                <i className='wi wi-degrees'></i> {this.props.isCelsius ? 'C' : 'F'}
-            </div>
-        );
-    }
-}
+const ScalePicker = props => (
+    <div className='scale-picker' onClick={props.handleChangeScale}>
+        <i className='wi wi-degress'></i> {props.isCelsius ? 'C' : 'F'}
+    </div>
+);
 
 export default ScalePicker;
