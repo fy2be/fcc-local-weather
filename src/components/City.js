9e84e8884e8a3ab47b41466cde5cf245fbc3700c
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-15 10:57:04 +0200

    City is now stateless Component

--- src/components/City.js
+++ src/components/City.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
-class City extends React.Component {
-    render() {
-        const data = this.props.data;
+const City = props => {
+    const { name, country } = props.data;
 
-        let cityString = 'Looking for your city...';
-        if (data) {
-            const city = this.props.data;
-            cityString = `${city.name}, ${city.country}`;
-        }
-
-        return <div className='city'>{cityString}</div>
-    }
-}
+    return (
+        <div className='city'>
+            {name}, {country}
+        </div>);
+};
 
 export default City;
