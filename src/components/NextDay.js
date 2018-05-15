293f16fbb82fdf0b6b1a9322f8367ba121c8ab8c
fy2be <36625528+fy2be@users.noreply.github.com>
2018-05-15 11:15:41 +0200

    NextDay is now stateless component

--- src/components/NextDay.js
+++ src/components/NextDay.js
@@ -1,40 +1,34 @@
 import React from 'react';
 import { celsiusToFahrenheit, formatDate, iconToClass } from '../helpers';
 
-class NextDay extends React.Component {
-    render() {
-        if (!this.props.weather) {
-            return <h3>Loading data</h3>;
-        }
+const NextDay = props => {
+    let { temp_min, temp_max } = props.weather.main;
+    let scaleClass = 'wi-celsius';
+    const icon = props.weather.weather[0].icon;
 
-        let { temp_min, temp_max } = this.props.weather.main;
-        let scaleClass = 'wi-celsius';
-        const icon = this.props.weather.weather[0].icon;
-
-        if (!this.props.isCelsius) {
-            temp_min = celsiusToFahrenheit(temp_min);
-            temp_max = celsiusToFahrenheit(temp_max);
-            scaleClass = 'wi-fahrenheit';
-        }
-
-        return (
-            <div className='next-day' onClick={() => this.props.handleChangeActiveDay(this.props.weather)}>
-                <div className='weather-icon'>
-                    <i className={`wi ${iconToClass(icon)}`}></i>
-                </div>
-
-                <div className='date'>
-                    {formatDate(this.props.weather.dt_txt)}
-                </div>
-
-                <div className='day-night'>
-                    <div className='max'>{temp_max.toFixed(1)} <i className={`wi ${scaleClass}`}></i></div>
-                    <div className='slash'>/</div>
-                    <div className='min'>{temp_min.toFixed(1)} <i className={`wi ${scaleClass}`}></i></div>
-                </div>
-            </div>
-        );
+    if (!props.isCelsius) {
+        temp_min = celsiusToFahrenheit(temp_min);
+        temp_max = celsiusToFahrenheit(temp_max);
+        scaleClass = 'wi-fahrenheit';
     }
-}
+
+    return (
+        <div className='next-day' onClick={() => props.handleChangeActiveDay(props.weather)}>
+            <div className='weather-icon'>
+                <i className={`wi ${iconToClass(icon)}`}></i>
+            </div>
+
+            <div className='date'>
+                {formatDate(props.weather.dt_txt)}
+            </div>
+
+            <div className='day-night'>
+                <div className='max'>{temp_max.toFixed(1)} <i className={`wi ${scaleClass}`}></i></div>
+                <div className='slash'>/</div>
+                <div className='min'>{temp_min.toFixed(1)} <i className={`wi ${scaleClass}`}></i></div>
+            </div>
+        </div>
+    );
+};
 
 export default NextDay;
